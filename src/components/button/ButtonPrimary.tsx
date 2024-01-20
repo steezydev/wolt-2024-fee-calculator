22ace0b25a8249064c8eab2159928a9dd755656f
Ivan Ilichev <thesteezydev@gmail.com>
2024-01-21 02:19:02 +0300

    update ButtonPrimary styles

--- src/components/button/ButtonPrimary.tsx
+++ src/components/button/ButtonPrimary.tsx
@@ -8,7 +8,7 @@ const ButtonPrimary = ({ className, ...props }: ButtonProps) => {
     <ButtonUnstyled
       {...props}
       className={classNames(
-        'bg-primary-300 text-white text-lg font-bold py-4 rounded-xl',
+        'bg-primary-300 text-white text-lg font-bold py-3 rounded-xl',
         'hover:bg-primary-300/90 active:bg-primary-300/80',
         'disabled:bg-primary-300/60 disabled:text-white/40 disabled:cursor-not-allowed',
         className
