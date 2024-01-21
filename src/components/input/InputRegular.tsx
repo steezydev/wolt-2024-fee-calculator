1c0d4c91de5f0911cab32550656fd9bd85e798a0
Ivan Ilichev <thesteezydev@gmail.com>
2024-01-21 19:02:38 +0300

    Update placeholder invalid styles

--- src/components/input/InputRegular.tsx
+++ src/components/input/InputRegular.tsx
@@ -10,7 +10,7 @@ const InputRegular = ({ className, ...props }: InputProps) => {
         'rounded-xl border-2 bg-transparent border-neutral-light-100 placeholder:text-neutral-light-300 px-3 py-2.5 text-xl text-black leading-none',
         ' dark:border-neutral-dark-100 dark:placeholder:text-neutral-dark-300 dark:text-white',
         'group-hover:data-[invalid=false]:border-primary-300',
-        //'data-[invalid=true]:placeholder:text-error data-[invalid=true]:dark:placeholder:text-error',
+        'data-[invalid=true]:placeholder:text-error data-[invalid=true]:dark:placeholder:text-error',
         'data-[invalid=true]:border-error data-[invalid=true]:dark:border-error focus:data-[invalid=true]:border-primary-300 focus:data-[invalid=true]:dark:border-primary-300',
         'focus:outline-none focus:ring-1 focus:ring-primary-300 focus:border-primary-300 focus:dark:border-primary-300',
         className
