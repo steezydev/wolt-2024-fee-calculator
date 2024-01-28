a3d23b66c03ebf687c6d86d8192c86eef03cd19c
Ivan Ilichev <thesteezydev@gmail.com>
2024-01-28 19:28:41 +0300

    Update button primary styles

--- src/components/button/ButtonPrimary.tsx
+++ src/components/button/ButtonPrimary.tsx
@@ -9,7 +9,7 @@ const ButtonPrimary = ({ className, ...props }: ButtonProps) => {
       className={classNames(
         'bg-primary-300 text-white text-lg font-bold py-3 rounded-xl',
         'hover:bg-primary-300/90 active:bg-primary-300/80',
-        'disabled:bg-primary-300/60 disabled:text-white/40 disabled:cursor-not-allowed',
+        'disabled:bg-neutral-700 disabled:text-white/60 disabled:cursor-not-allowed',
         'focus-visible:ring-4 focus-visible:ring-focus focus:outline-none focus-visible:outline-none',
         className
       )}
